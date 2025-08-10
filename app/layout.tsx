dd33fe31f086ee4b262e8142202841cb3fd89d8e
nodownloadgames.github.io <dutinglove@gmail.com>
2025-08-10 08:18:43 +0800

    Add Microsoft Clarity tracking code

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,24 @@ export default function RootLayout({
 }) {
   return (
     <html>
+      <head>
+        {/* Google Search Console 验证 */}
+        <meta name="google-site-verification" content="google11a490ed640e7fc6" />
+        
+        {/* Microsoft Clarity */}
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "ssdsoc827u");
+            `,
+          }}
+        />
+      </head>
       <body>
         {children}
       </body>
